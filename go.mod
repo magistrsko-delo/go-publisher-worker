96040886c91ca34a505592dbf8b86214d0119bd4
Uros Zoretic <uz1734@student.uni-lj.si>
2020-05-30 15:29:26 +0200

    publisher worker health-checks

--- go.mod
+++ go.mod
@@ -5,7 +5,9 @@ go 1.13
 require (
 	github.com/dustin/go-humanize v1.0.0
 	github.com/golang/protobuf v1.4.0
+	github.com/heptiolabs/healthcheck v0.0.0-20180807145615-6ff867650f40 // indirect
 	github.com/joho/godotenv v1.3.0 // indirect
+	github.com/prometheus/client_golang v1.6.0 // indirect
 	github.com/streadway/amqp v0.0.0-20200108173154-1c71cc93ed71
 	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e
 	google.golang.org/grpc v1.28.1
--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"github.com/heptiolabs/healthcheck"
 	"github.com/joho/godotenv"
 	"go-publisher-worker/Models"
 	"go-publisher-worker/Worker"
 	"log"
 	"net/http"
+	"time"
 )
 
 func init()  {
@@ -18,6 +20,18 @@ func init()  {
 
 func main()  {
 	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	env := Models.GetEnvStruct()
+	health := healthcheck.NewHandler()
+
+	health.AddLivenessCheck("timeshift: " + env.TimeShiftGrpcServer + ":" + env.TimeShiftGrpcPort, healthcheck.TCPDialCheck(env.TimeShiftGrpcServer + ":" + env.TimeShiftGrpcPort, 5*time.Second))
+	health.AddLivenessCheck("aws-storage: " + env.AwsStorageGrpcServer + ":" + env.AwsStorageGrpcPort, healthcheck.TCPDialCheck(env.AwsStorageGrpcServer + ":" + env.AwsStorageGrpcPort, 5*time.Second))
+	health.AddLivenessCheck("media-metadata: " + env.MediaMetadataGrpcServer + ":" + env.MediaMetadataGrpcPort, healthcheck.TCPDialCheck(env.MediaMetadataGrpcServer + ":" + env.MediaMetadataGrpcPort, 5*time.Second))
+	health.AddLivenessCheck("sequence-service: " + env.SequenceServiceServer + ":" + env.SequenceServicePort, healthcheck.TCPDialCheck(env.SequenceServiceServer + ":" + env.SequenceServicePort, 5*time.Second))
+	health.AddLivenessCheck("chunk-metadata: " + env.ChunkMetadataGrpcServer + ":" + env.ChunkMetadataGrpcPort, healthcheck.TCPDialCheck(env.ChunkMetadataGrpcServer + ":" + env.ChunkMetadataGrpcPort, 5*time.Second))
+	health.AddLivenessCheck("rabbit-mq: " + env.RabbitHost + ":" + env.RabbitPort, healthcheck.TCPDialCheck(env.RabbitHost + ":" + env.RabbitPort, 5*time.Second))
+
+	go http.ListenAndServe("0.0.0.0:8888", health)
+
 	worker := Worker.InitWorker()
 	defer worker.RabbitMQ.Conn.Close()
 	defer worker.RabbitMQ.Ch.Close()
